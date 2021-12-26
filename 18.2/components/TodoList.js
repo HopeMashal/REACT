2360ee34c7fd0c6ea434cbbf45fdcd0890ead70f
HopeMashal <mashal.hope@gmail.com>
2021-12-26 16:24:20 +0200

    edit 18.2

--- 18.2/components/TodoList.js
+++ 18.2/components/TodoList.js
@@ -9,7 +9,6 @@ const ToDoList = (props) => {
 
     return (
         <div className="todo-list">
-            {/* <span className={isCompleted && "completed"}><del>{props.text}</del></span>{" "} */}
             <div role={"button"} onClick={handleClick} className="btn">
                 {isCompleted ? <span><del>{props.text}</del> <span style={{cursor:'pointer'}}>&#10004;</span></span> : <span>{props.text} <span style={{cursor:'pointer'}}>&#10008;</span></span>}
             </div>
